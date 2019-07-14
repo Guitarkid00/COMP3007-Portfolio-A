770931c47ccc05e3598dfd2d0d949801f319d6bc
Layton <guitarkid00000@gmail.com>
2019-07-14 19:58:11 +1200

    First Commit

---
+++ Portfolio Part 1 - Roll-A-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    private int count;
+    public Text countText;
+    public Text winText;
+
+    private void Start()
+    {
+        winText.text = "";
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+    }
+
+    void FixedUpdate ()
+    {
+        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        
+        Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+        rb.AddForce(movement * speed);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 10;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score : " + count.ToString();
+        if (count >= 60)
+        {
+            winText.text = "You Win!!!";
+        }
+    }
+}
